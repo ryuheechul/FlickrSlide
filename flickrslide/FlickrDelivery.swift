50429bc41307d3a83fe0d09c5e92ac99d0b7b55d
Ryu Heechul <ryuhcii@gmail.com>
2016-08-20 12:13:24 -0700

    adjust interval. play/pause.

--- flickrslide/FlickrDelivery.swift
+++ flickrslide/FlickrDelivery.swift
@@ -12,24 +12,36 @@ import Haneke
 class FlickrDelivery: NSObject {
 
     func get(page: Int, completion: (urls: Array<String>) -> Void ) {
-        let cache = Cache<JSON>(name: "github")
+        let cache = Cache<JSON>(name: "flickr")
         let urlString = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&page=\(page)"
 
-        print(urlString)
         let URL = NSURL(string: urlString)!
 
-
         cache.fetch(URL: URL).onSuccess { JSON in
-            var arr = [String]()
+            print(JSON)
+            var result = [String]()
 
-            let ab: Array? = (JSON.dictionary?["items"] as! NSArray) as Array
+            let _items: Array? = (JSON.dictionary?["items"] as! NSArray) as Array
 
-            for a in ab! {
-                let url:String = (a["media"]!)!["m"] as! String
-                arr.append(url)
+            if let items = _items {
+                for item in items {
+
+                    let _media: Dictionary? = (item["media"] as! NSDictionary) as Dictionary
+
+                    if let media = _media {
+
+                        let _url:String? = media["m"] as? String
+
+                        if let url = _url {
+                            result.append(url)
+                        }
+                    }
+
+                }
+
             }
 
-            completion(urls: arr)
+            completion(urls: result)
         }
     }
 }
--- flickrslide/ViewController.swift
+++ flickrslide/ViewController.swift
@@ -11,7 +11,10 @@ import Haneke
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toolbar: UIToolbar!
+
     @IBOutlet var imgV : UIImageView?
+    var tf:UITextField?
     var delivery: FlickrDelivery?
     var queue: [String] = []
     var timer = NSTimer()
@@ -19,8 +22,34 @@ class ViewController: UIViewController {
     var page = 0
     var isShowing = true
 
-    @IBAction func startPause(sender: UIBarButtonItem) {
-        sender.sy
+    @IBAction func buttonTapped(sender: AnyObject) {
+        if tf?.isFirstResponder() == true {
+            tf?.resignFirstResponder()
+            return
+        }
+
+        timer.invalidate()
+        self.navigationController?.navigationBar.hidden = false
+        toolbar.hidden = false
+    }
+
+    func interval() -> Double {
+        let intervalText = tf?.text
+        var interval = Double(intervalText!)
+
+        if interval == 0 {
+            interval = 1
+        }
+
+        return interval!
+    }
+
+    @IBAction func start(sender: UIBarButtonItem) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(interval(), target: self, selector: #selector(ViewController.showNext), userInfo: nil, repeats: true)
+        showNext()
+
+        self.navigationController?.navigationBar.hidden = true
+        toolbar.hidden = true
     }
 
     override func viewDidLoad() {
@@ -32,7 +61,41 @@ class ViewController: UIViewController {
             self.fetchPhotos()
         }
 
-        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.showNext), userInfo: nil, repeats: true)
+        let titlebar:UIView? = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 40))
+        tf = UITextField(frame: CGRect(x: 0, y: 0, width: 30, height: 40))
+        let lbl:UILabel? = UILabel(frame: CGRect(x: 30, y: 0, width: 20, height: 40))
+        tf?.keyboardType = UIKeyboardType.NumberPad
+        tf?.returnKeyType = UIReturnKeyType.Done
+
+        lbl?.text = "s"
+        tf?.placeholder = "1"
+        tf?.text = "1"
+
+        titlebar?.addSubview(tf!)
+        titlebar?.addSubview(lbl!)
+
+        tf?.addTarget(self, action: #selector(ViewController.textDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+
+        self.navigationController?.navigationBar.topItem?.titleView = titlebar!
+    }
+
+    func textDidChange(textField: UITextField) {
+        let text = textField.text
+        let textNum = Int(text!)
+
+        if textNum > 0 && textNum < 11 {
+            if textNum > 1 {
+                tf?.resignFirstResponder()
+            }
+            return
+        }
+
+        else if textNum < 1 {
+//            textField.text = "1"
+        }
+        else if textNum > 10 {
+            textField.text = "10"
+        }
     }
 
     func fetchPhotos() {
@@ -41,10 +104,12 @@ class ViewController: UIViewController {
         }
 
         isFetching = true
+
         delivery?.get(page, completion: { (urls) in
             self.queue.insertContentsOf(urls.reverse(), at: 0)
             self.isFetching = false
         })
+
         page+=1
     }
 
@@ -65,7 +130,19 @@ class ViewController: UIViewController {
         }
 
         if let url = NSURL(string: urlString!) {
-            imgV?.hnk_setImageFromURL(url)
+
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), { 
+                self.imgV?.hnk_setImageFromURL(url)
+            })
+            UIView.animateWithDuration(0.5, animations: {
+                self.imgV?.alpha = 0
+            }, completion: { (Bool) in
+
+                UIView.animateWithDuration(0.5, animations: {
+                    self.imgV?.alpha = 1
+                })
+            })
+
         }
     }
 
